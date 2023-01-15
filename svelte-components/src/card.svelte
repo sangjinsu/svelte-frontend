ea40829c83a7a536462f54ad88bd9c05d5f4c2d6
sangjinsu <climbplant39@kakao.com>
2023-01-15 20:07:13 +0900

    ✨ 슬롯

---
+++ svelte-components/src/card.svelte
@@ -0,0 +1,19 @@
+<script>
+   let hovering = 9
+</script>
+
+<style>
+    .card {
+        width: 300px;
+        border: 1px solid #aaa;
+        border-radius: 2px;
+        box-shadow: 2px 2px 8px rgba(0, 0, 0, 0.1);
+        padding: 1em;
+        margin: 0 0 1em 0;
+    }
+</style>
+
+<div class="card">
+    <slot></slot>
+    <slot name="hover" {hovering}></slot>
+</div>
